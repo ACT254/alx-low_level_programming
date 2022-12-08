d8270f19dcea89da505106f46e250efeb5687ed0
Elvis Likoko <elviswanasunia11568@gmail.com>
2022-12-08 10:29:14 -0800

    betty documentation

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - This the entry point for the program
+ * Return: return 0 if program is successful
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
