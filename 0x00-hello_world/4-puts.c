87bb47d26eee825cc7bf0054e95984412c8dc452
Elvis Likoko <elviswanasunia11568@gmail.com>
2022-12-08 10:06:31 -0800

    correation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
-#include <stdio.h>
 /**
- * This function prints the string shown below
+ * This is the documentaion of the function
+ * The function basically prints the following string
  * "Programming is like building a multilingual puzzle
  */
+
+
+#include <stdio.h>
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
