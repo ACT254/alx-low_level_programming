d3ba54eb3bc91865224ad4c45ddd5d8b740f6447
Elvis Likoko <elviswanasunia11568@gmail.com>
2022-12-08 10:21:49 -0800

    betty documentaion

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
-/**
- * This is the documentaion of the function
- * The function basically prints the following string
- * "Programming is like building a multilingual puzzle
- */
-
-
 #include <stdio.h>
+/**
+ * main - entry point of the program
+ * Return - Returns a value of zero
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
