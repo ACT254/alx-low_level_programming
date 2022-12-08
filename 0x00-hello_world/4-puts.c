42dbeefddfde4bd46e5670e6310056e63c869ec1
Elvis Likoko <elviswanasunia11568@gmail.com>
2022-12-08 10:02:48 -0800

    betty documentaion

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * This function prints the string shown below
+ * "Programming is like building a multilingual puzzle
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
