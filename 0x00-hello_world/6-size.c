73fe467fcc2840f5c805f5771aef156676865a1f
Elvis Likoko <elviswanasunia11568@gmail.com>
2022-12-08 10:48:05 -0800

    getting sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	printf("Size of a char: %lu bytes(s)\n",sizeof(a));
+	printf("Size of an int: %lu bytes(s)\n", sizeof(b));
+	printf("Size of long int: %lu bytes(s)\n",sizeof(c));
+	printf("Size of long long int: %lu bytes(s)\n",sizeof(d));
+	printf("Size of a float: %lu byte(s)\n",sizeof(e));
+	return (0);
+}
