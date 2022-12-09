d10488934b09a195e6438dfbd8fb0e148ec7ec6e
Elvis Likoko <elviswanasunia11568@gmail.com>
2022-12-09 14:55:42 -0800

    betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ int main()
 	char d;
 	float f;
 	
-	printf("Size of char: %lu byte(s)\n",
+	printf("Size of a char: %lu byte(s)\n",
 			(unsigned long)(sizeof(d)));
 	printf("Size of an int: %lu byte(s)\n",
 			(unsigned long)sizeof(a));
