7d0411423ea957c8dfb9b09fba628f8c216d1de1
Elvis Likoko <elviswanasunia11568@gmail.com>
2022-12-10 05:40:32 -0800

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,11 +6,10 @@
 int main(void)
 {
 	int n = 0;
-	char st[] = "and that piece of art is useful\" - Dora Korpar, 2015-1									0-19\n";
+	char st[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	while (st[n] != '\0')
 	{
 		putchar(st[n]);
 		n++;
-
 	}
 }
