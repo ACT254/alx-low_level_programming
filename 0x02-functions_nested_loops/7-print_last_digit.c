95e4d2997526e76f988ef719a7e0e87e92533dc2
Elvis Likoko <elviswanasunia11568@gmail.com>
2022-12-14 11:40:22 -0800

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,13 +8,23 @@
   */
 int print_last_digit(int n)
 {
+	int last_digit;
+
 	if (n >= 0)
 	{
-		return (n % 10);
+		last_digit = n % 10;
+		_putchar(last_digit);
+		_putchar(last_digit);
+		_putchar('\n');
+
 	}
 	else
 	{
-		return (-1 * (n % 10));
+		last_digit = (-1 * (n % 10));
+		_putchar(last_digit);
+		_putchar(last_digit);
+		_putchar('\n');
+
 	}
 }
 
