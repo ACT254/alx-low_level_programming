1517759ec531a2022c5d5b0d9459f24b1fa406ee
Elvis Likoko <elviswanasunia11568@gmail.com>
2022-12-14 11:04:18 -0800

    checking the signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_sign - checking the sign of a number
+  * @n: the number whose sign is to be checked
+  *
+  * Return: 1, 0 or -1 depending on the number
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else
+		return (-1);
+}
