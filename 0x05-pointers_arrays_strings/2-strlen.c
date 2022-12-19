b3dc80d9727b3878d192951ad2ee5cd657de461c
Elvis Likoko <elviswanasunia11568@gmail.com>
2022-12-19 09:07:32 -0800

    length of a strign

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _strlen - checking the string length
+  * @s: the string to be checked
+  *
+  * Return: returns the length of the string
+  */
+
+int _strlen(char *s)
+{
+	int n = 0;
+
+	while(*s[n] != '\0')
+	{
+		n += 1;
+	}
+
+	return (n + 1);
+}
