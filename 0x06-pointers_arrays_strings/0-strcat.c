a602cdb007bc1243c5b06d167402b672f962c961
Elvis Likoko <elviswanasunia11568@gmail.com>
2022-12-22 13:10:23 -0800

    string concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * _strcat - concatenates two strings
+  * @dest: the destination string
+  * @src: pointer to the  string to be appended at the end of dest
+  *
+  * Return: a pointer to the resulting string dest
+  */
+
+char *_strcat(char *dest, char *src)
+{
+	/* determining the end of the first string */
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	/* concatenating the second string */
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	/* adding the null terminator */
+	*end = '/0';
+
+	return (dest);
+}
