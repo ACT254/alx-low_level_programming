8d30ddae2416d1a639e302575e6e247aa5c6d341
Elvis Likoko <elviswanasunia11568@gmail.com>
2023-01-03 06:56:05 -0800

    memory copy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,14 +6,14 @@
   * @src: a pointer to the source
   * @n: number of bytes to be copied
   *
-  * Return: a pointer to des
+  * Return: a pointer to dest
   */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++;)
+	for (i = 0; i < n; i++)
 	{
 		*(dest + i) = *(src + i)
 	}
