09db2d237d38d2e36e9f4692ca0478274eb6ab66
Elvis Likoko <elviswanasunia11568@gmail.com>
2023-01-03 06:42:01 -0800

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,13 +11,13 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (i <= (n - 1))
+	for (i = 0; i < n; i++)
 	{
 		*(s + i) = b;
 		i++;
 	}
 
 	return (s);
-i}
+}
