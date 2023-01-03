3b5eed642f0949a3c7ccb2606c81e6a13024ef86
Elvis Likoko <elviswanasunia11568@gmail.com>
2023-01-03 07:12:06 -0800

    first character occurence

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * _strchr - returns pointer to the first ocurrence of character
+  * @s: the string to be checked
+  * @c: the character to be checked
+  *
+  * Return: pointer to the first occurence of the character
+  */
+
+char *_strchr(char *s, char c)
+{
+	int n = 0;
+
+	while (*(s + n) != '\0')
+	{
+		if (*(s + n) == c)
+		{
+			return ((s + n));
+			break;
+		}
+
+	}
+}
