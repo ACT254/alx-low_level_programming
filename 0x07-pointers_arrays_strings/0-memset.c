84032fda72f40d351b314d71bee29eff513cb6ea
Elvis Likoko <elviswanasunia11568@gmail.com>
2023-01-03 06:05:54 -0800

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * _memset - fills the first n bytes of the memory area pointed to by s with the constant byte b
+  * s: a pointer to character
+  * @b: the constant byte
+  * @n: an integer n
+  *
+  * Return: a pointer to the memory area s
+  */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+	char *m;
+
+	while (i <= (n -1))
+	{
+		*(s + i) = b;
+		i++;
+	}
+
+	m = &s;
+
+	return (m);
+}
+
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,7 @@
+# More Pointers, Arrays and Strings
+
+This project explores more pointers, arrays and strings. It enables one to get more practice on this concepts.
+
+## Task 0
+
+Writing a function that fills the memoery with a constant byte.
