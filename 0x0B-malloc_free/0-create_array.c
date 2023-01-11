b818b1be79b73802e8b86a62e5e46ba2a5e11ee0
Elvis Likoko <elviswanasunia11568@gmail.com>
2023-01-11 13:34:46 -0800

    corrections

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,15 +10,19 @@
 
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
+	char *ptr;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
-
-	char *ptr;
 	ptr = malloc(size * sizeof(char));
 
-	ptr[0] = c;
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
 
 	return (ptr);
 }
