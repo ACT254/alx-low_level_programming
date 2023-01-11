d143f55b7bbd3f1b243b0d25e5a132fdc610bad6
Elvis Likoko <elviswanasunia11568@gmail.com>
2023-01-11 13:37:36 -0800

    fixing the malloc return value

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,9 +15,13 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 	{
-		return (NULL);
+		return (0);
 	}
 	ptr = malloc(size * sizeof(char));
+	if (ptr == NULL)
+	{
+		return (0);
+	}
 
 	for (i = 0; i < size; i++)
 	{
