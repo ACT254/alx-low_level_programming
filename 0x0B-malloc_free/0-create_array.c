ac195f6707b83a70ddc67458762fd8fcf0a3e637
Elvis Likoko <elviswanasunia11568@gmail.com>
2023-01-11 13:24:40 -0800

    create dynamic size array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+/**
+  * create_array - creates an array and gives the initial character
+  * @size: the size of the array
+  * @c: the character for initialization
+  *
+  * Return: pointer to array or NULL is allocation not successful
+  */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	char *ptr = (char *)malloc(size * sizeof(char));
+
+	ptr[0] = c;
+
+	return (ptr);
+}
