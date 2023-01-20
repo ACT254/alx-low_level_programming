628af2dfa0096e7e9eacba470c77554e7354d33d
Elvis Likoko <elviswanasunia11568@gmail.com>
2023-01-19 23:30:54 -0800

    print name function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+  * print_name -  function that prints a name
+  * @name: pointer to the name to be printed
+  * @f: the pointer to the function to be used
+  *
+  * Retunrn: nothing
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+	{
+		if (name != NULL)
+		{
+			f(name);
+		}
+	}
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+# Function Pointers
+This project deals with pointers which point to functions. The pointers specifically point to the function code. There are several tasks to test the understanding of these function pointers.
+
