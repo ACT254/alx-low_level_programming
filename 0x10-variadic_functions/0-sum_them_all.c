09b8babc1a75012217671772d2fafca4e38e3fe3
Elvis Likoko <elviswanasunia11568@gmail.com>
2023-01-24 03:57:46 -0800

    correction of the fucntion logic

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,30 +1,27 @@
 #include "variadic_functions.h"
 /**
-  * sum_them_all - gets the sum of all the parameters
-  * @n: the number of values to be added
-  *
-  * Return: the sum of the numbers
-  */
-
+ * sum_them_all - function that sum of all its parameters
+ *
+ * @n: How many variables will be inserted in the function
+ *
+ * Return: return the sum of all its parameters
+ */
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list list;
-
-	unsigned int i; sum = 0;
+	unsigned int i, sum = 0;
 
 	va_start(list, n);
 
 	if (n == 0)
+	{
 		return (0);
-
+	}
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(list, int);
+		sum = sum + va_arg(list, int);
 	}
 
 	va_end(list);
-
 	return (sum);
 }
-
-
